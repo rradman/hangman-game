cc167a0288211382fd6012035887fa485253c63d
rradman <55345695+rradman@users.noreply.github.com>
2019-09-20 22:48:54 +0200

    Update and rename skripta.sh to build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get -y install lamp-server^
 sudo chmod 777 /var/www/html
-sudo git clone https://robi92@bitbucket.org/robi92/public.git
+https://github.com/rradman/hangman-game.git
 sudo cp -r ~/public/Hangman/* /var/www/html
 sudo mysql --user=root --password=root -e "create database projekt";
 sudo mysql --user=root --password=root --default_character_set=utf8 projekt < /var/www/html/projekt.sql
